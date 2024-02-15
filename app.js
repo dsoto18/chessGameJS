0abd253bbf5ff96d68e571c0b4ecca407eab1d46
diegosoto9744@gmail.com <you@example.com>
2024-02-15 18:29:30 -0500

    Drag and drop functionality added

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ function createBoard(){
         const square = document.createElement('div')
         square.classList.add('square')
         square.innerHTML = startPiece
+        square.firstElementChild?.setAttribute('draggable', true)
         square.setAttribute('square-id', i)
         const row = Math.floor( (63 - i) / 8) + 1
         if( row % 2 === 0){
@@ -26,17 +27,40 @@ function createBoard(){
         } else {
             square.classList.add(i % 2 === 0 ? "brown" : "beige" )
         }
-
         if( i <= 15 ){
             square.firstChild.firstChild.classList.add('black')
         }
-
         if( i >= 48){
             square.firstChild.firstChild.classList.add('white')
         }
-
         gameboard.append(square)
     })
 }
-
 createBoard()
+
+const allSquares = document.querySelectorAll("#gameboard .square")
+
+allSquares.forEach(square => {
+    square.addEventListener('dragstart', dragStart)
+    square.addEventListener('dragover', dragOver)
+    square.addEventListener('drop', dragDrop)
+})
+
+let startPositionId
+let draggedElement 
+
+function dragStart(e){
+    startPositionId = e.target.parentNode.getAttribute('square-id')
+    draggedElement = e.target
+}
+
+function dragOver(e){
+    e.preventDefault()
+}
+
+function dragDrop(e){
+    e.stopPropagation()
+    e.target.parentNode.append(draggedElement)
+    e.target.remove()
+    //e.target.append(draggedElement)
+}
